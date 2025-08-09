226d5601a56530fd954e826cda540af838c4432a
KlemensasKaubrys <kaubrys.klemensas@outlook.com>
2025-08-10 00:55:45 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
 set -ouex pipefail
-
-dnf5 install -y rocminfo rocm-opencl rocm-clinfo rocm-hip
-
 ls -ld /opt || true
 ls -ld /var/opt || true
 namei -l /opt || true
@@ -14,9 +10,11 @@ fi
 mkdir -p /var/opt
 
 rpm --import https://repos.fyralabs.com/terra$(rpm -E %fedora)/key.asc
-dnf5 -y install --repofrompath=terra,https://repos.fyralabs.com/terra$(rpm -E %fedora)/ terra-release zed
 
+dnf5 install -y --repofrompath=terra-bootstrap,https://repos.fyralabs.com/terra$(rpm -E %fedora)/ terra-release
 
+dnf5 install -y zed
+dnf5 install -y rocminfo rocm-opencl rocm-clinfo rocm-hip
 dnf5 install -y brave-browser
 
 systemctl enable podman.socket
