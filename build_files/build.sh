dc5eed6d21edc07a568e1ec478bcb54633b28948
KlemensasKaubrys <kaubrys.klemensas@outlook.com>
2025-08-10 01:04:18 +0300

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,12 +9,21 @@ fi
 [ -L /opt ] || ln -s ../var/opt /opt
 mkdir -p /var/opt
 
-rpm --import https://repos.fyralabs.com/terra$(rpm -E %fedora)/key.asc
+rpm --import "https://repos.fyralabs.com/terra$(rpm -E %fedora)/key.asc"
 
-dnf5 install -y --repofrompath=terra-bootstrap,https://repos.fyralabs.com/terra$(rpm -E %fedora)/ terra-release
+dnf5 -y install \
+  --repofrompath=terra-bootstrap,"https://repos.fyralabs.com/terra$(rpm -E %fedora)/" \
+  terra-release
 
-dnf5 install -y zed
-dnf5 install -y rocminfo rocm-opencl rocm-clinfo rocm-hip
-dnf5 install -y brave-browser
+sed -i -e 's/^enabled=.*/enabled=1/' \
+       -e "s|^baseurl=.*|baseurl=https://repos.fyralabs.com/terra$(rpm -E %fedora)/\$basearch/|" \
+       /etc/yum.repos.d/terra.repo || true
+
+dnf5 --refresh repoquery --repo terra 'zed*' || true
+
+dnf5 -y --refresh install zed
+dnf5 -y install rocminfo rocm-opencl rocm-clinfo rocm-hip
+dnf5 -y install brave-browser
+
 
 systemctl enable podman.socket
